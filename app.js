38426801fd19885fa7dd8c1d8c036c94dbf4cae8
tinoe0404 <tinoe044@gmail.com>
2025-06-06 11:23:26 +0200

    added views and layouts

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const expressLayouts = require('express-ejs_layours');
 const app = express();
-const PORT = process.env.PORT || 5000;
 
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
